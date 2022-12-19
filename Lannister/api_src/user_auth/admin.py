1de56375440417853d9d04a12f2f770a125939e9
Neteli Maksymuk <maksymuknatalia@gmail.com>
2022-12-19 17:39:55 +0200

    Finnished register user

--- Lannister/api_src/user_auth/admin.py
+++ Lannister/api_src/user_auth/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 # psql -h localhost -U postgres
 # Register your models here.
+from .models import UserModel
+
+admin.site.register(UserModel)
--- Lannister/api_src/user_auth/models.py
+++ Lannister/api_src/user_auth/models.py
@@ -15,13 +15,38 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, name, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(name=name, email=self.normalize_email(email), roles=['admin']
+        )
+        user.is_admin = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 
 class UserModel(AbstractBaseUser):
     name = models.CharField(max_length=80, unique=True)
     email = models.EmailField(verbose_name='email address', max_length=40, unique=True)
     roles = ArrayField(models.CharField(max_length=8), blank=True)
+    is_admin = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'name'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['email']
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
 
     objects = UserManager()
